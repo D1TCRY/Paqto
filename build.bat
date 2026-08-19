88b95d00d81c5a7fb94ec6155a26483712bbf7eb
Diego Cecchelani <diego.cecchelani@outlook.it>
2026-08-19 12:07:28 +0200

    ...5

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-python -m pip install --upgrade build
-python -m build
-pause
--- publish.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-python -m pip install --upgrade twine
-python -m twine upload dist/* --verbose
-pause
